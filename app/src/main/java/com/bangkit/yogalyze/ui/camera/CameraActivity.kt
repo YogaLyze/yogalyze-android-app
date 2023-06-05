2127929b8a0a5a9ecf958dddf4b98d8c98a6da3e
imam <imamwaliyuddinrabbani@gmail.com>
2023-06-05 14:46:51 +0700

    Commit

--- app/src/main/java/com/bangkit/yogalyze/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/bangkit/yogalyze/ui/camera/CameraActivity.kt
@@ -180,6 +180,7 @@ package com.bangkit.yogalyze.ui.camera
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.util.Size
@@ -200,6 +201,11 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.bangkit.yogalyze.R
 import com.bangkit.yogalyze.databinding.ActivityCameraBinding
+import com.bangkit.yogalyze.ml.BackPain
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -209,6 +215,9 @@ class CameraActivity : AppCompatActivity() {
     private lateinit var cameraExecutor: ExecutorService
     private lateinit var poseName: String
     private lateinit var yogaName: String
+    lateinit var model1: BackPain
+    lateinit var labels: List<String>
+    var outputFeature0: FloatArray? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -230,23 +239,25 @@ class CameraActivity : AppCompatActivity() {
         binding.poseImageView.setImageResource(intent.getIntExtra(EXTRA_IMAGE, 0))
         poseName = intent.getStringExtra(EXTRA_POSE).toString()
         yogaName = intent.getStringExtra(EXTRA_YOGA).toString()
-
+        labels = FileUtil.loadLabels(this, "labelBackPain.txt")
+        model1 = BackPain.newInstance(this)
         val textureView: TextureView = findViewById(R.id.textureView)
         val previewView: PreviewView = findViewById(R.id.camera)
-        textureView.post { startCamera(previewView) }
+        textureView.post { startCamera(previewView,textureView) }
     }
 
-    private fun startCamera(previewView: PreviewView) {
+    private fun startCamera(previewView: PreviewView, textureView: TextureView) {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraProviderFuture.addListener({
             val cameraProvider = cameraProviderFuture.get()
-            bindCameraUseCases(cameraProvider, previewView)
+            bindCameraUseCases(cameraProvider, previewView, textureView)
         }, ContextCompat.getMainExecutor(this))
     }
 
     private fun bindCameraUseCases(
         cameraProvider: ProcessCameraProvider,
-        textureView: PreviewView
+        textureView: PreviewView,
+        tv: TextureView
     ) {
         val preview = Preview.Builder()
             .setTargetResolution(Size(textureView.width, textureView.height))
@@ -260,16 +271,39 @@ class CameraActivity : AppCompatActivity() {
             .build()
 
         val poseDetectorAnalyzer = ImageAnalysis.Analyzer { image ->
-            val poseDetected = true
+            var poseDetected = true
 
             // Perbarui nilai timer jika pose terdeteksi
             if (poseDetected) {
                 runOnUiThread {
-                    binding.timer.text = "10" // Ubah teks menjadi "10"
+                    val bitmap = image.toBitmap()
+                    val gambar = Bitmap.createScaledBitmap(bitmap, 100, 100, false)
+                    val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 100, 100, 3), DataType.FLOAT32)
+                    val tensorImage = TensorImage(DataType.FLOAT32)
+                    tensorImage.load(gambar)
+                    val byteBuffer = tensorImage.buffer
+                    inputFeature0.loadBuffer(byteBuffer)
+                    val outputs1 = model1.process(inputFeature0)
+                    outputFeature0 = outputs1.outputFeature0AsTensorBuffer.floatArray
+                    val threshold = 0.5f
+                    val detectedScores = mutableListOf<Float>()
+
+                    outputFeature0?.forEachIndexed { index, score ->
+                        if (score > threshold) {
+                            binding.accuracyTextView.text = score.toString()
+
+                        }
+                    }
+                    if(gambar!=null){
+                        binding.timer.text = "10"
+                        poseDetected=false
+                    }
+                     // Ubah teks menjadi "10"
                 }
             }
-
-            image.close()
+            if(!poseDetected){
+                image.close()
+            }
         }
 
         imageAnalysis.setAnalyzer(cameraExecutor, poseDetectorAnalyzer)
