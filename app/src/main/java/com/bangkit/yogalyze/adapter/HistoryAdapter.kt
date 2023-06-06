dd34af786944e252f85883b5b1b7901ff00d1e9d
salsha08 <salshathaulinzky@gmail.com>
2023-06-06 15:03:53 +0700

    update code

--- app/src/main/java/com/bangkit/yogalyze/adapter/HistoryAdapter.kt
+++ app/src/main/java/com/bangkit/yogalyze/adapter/HistoryAdapter.kt
@@ -1,24 +1,35 @@
 package com.bangkit.yogalyze.adapter
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bangkit.yogalyze.R
 import com.bangkit.yogalyze.api.response.HistoryResponse
 import com.bangkit.yogalyze.model.Pose
 
-class HistoryAdapter(private val listPose: ArrayList<HistoryResponse>) : RecyclerView.Adapter<HistoryAdapter.ListViewHolder>() {
-    class ListViewHolder {
+class HistoryAdapter(private val listHistory: ArrayList<HistoryResponse>) : RecyclerView.Adapter<HistoryAdapter.ListViewHolder>() {
+    class ListViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val yogaName: TextView = itemView.findViewById(R.id.tv_yoga_title)
+        val poseName: TextView = itemView.findViewById(R.id.tv_yoga_pose)
+        val date : TextView = itemView.findViewById(R.id.yogaDate)
+        val accuracy : TextView = itemView.findViewById(R.id.accuracyTextView)
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        TODO("Not yet implemented")
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.history_item_view, parent, false)
+        return HistoryAdapter.ListViewHolder(view)
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return listHistory.size
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
     }
 }
